c03ebaf13643e5b4c8363078d833899705f18d41
spencerhill97 <spencerahill97@gmail.com>
2023-07-17 16:22:17 -0500

    still trying to resolve deployment issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./components/**/*.{html,js}", "./index.html"],
+  content: ["./src/**/*.{html, js, jsx}", "./src/**/*"],
   theme: {
     extend: {},
   },
