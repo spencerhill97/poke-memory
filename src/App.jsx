ab77d8a48c74a3638903bd3471e0d788f32d59dc
spencerhill97 <spencerahill97@gmail.com>
2023-07-17 15:31:45 -0500

    finished footer and done with this web app for now

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Gameboard from "./components/Gameboard";
 import WinningModal from "./components/WinningModal";
 import { useGlobalContext } from "./context/GlobalContext";
 import LosingModal from "./components/LosingModal";
+import Footer from "./components/Footer";
 
 function App() {
   const { isGameOver, isLevelUpdated } = useGlobalContext();
@@ -12,8 +13,9 @@ function App() {
       <Navbar />
       <Scoreboard />
       <Gameboard />
+      <Footer />
       {!isLevelUpdated && <WinningModal />}
-      {!isGameOver && <LosingModal />}
+      {isGameOver && <LosingModal />}
     </main>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <a
+        className="footer__link"
+        href="https://www.flaticon.com/free-icons/loser"
+        title="loser icons"
+        target="_blank"
+      >
+        Loser icons created by surang - Flaticon
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
